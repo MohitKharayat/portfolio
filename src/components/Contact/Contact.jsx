dd53973542f5e4b40ab0cf1d4714ed6a43bd4558
Mohit Kharayat <mohitkharayat0@gmail.com>
2024-07-31 14:46:02 +0530

    contact details update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,11 +20,11 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/mohit-kharayat/">linkedin.com/mohitkharayat</a>
+          <a href="https://www.linkedin.com/in/mohit-kharayat/" target="_blank">linkedin.com/mohitkharayat</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/MohitKharayat">github.com/mohitkharayat</a>
+          <a href="https://github.com/MohitKharayat" target="_blank">github.com/mohitkharayat</a>
         </li>
       </ul>
     </footer>
