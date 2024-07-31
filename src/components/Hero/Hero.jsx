389244e3bca5ef118ce9cbe3ec551aaf4c156436
Mohit Kharayat <mohitkharayat0@gmail.com>
2024-07-31 13:45:15 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from "./Hero.module.css";
+import { getImageUrl } from '../../utils';
+
+export const Hero = () => {
+  return (
+  <section className={styles.container}>
+    <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I am Mohit Kharayat</h1>
+        <p>
+            I am a computer science student, a full stack developer and a tech enthusiast. My educational journey has equipped me with a strong foundation in computer science, and I am passionate about staying on the cutting edge of technology.
+        </p>
+        <br />
+        <a href="mailto:mohitkharayat0@gmail.com" className={styles.contactBtn}>Contact me</a>
+        <br />
+        <a href="https://drive.google.com/uc?export=download&id=1g43rdcP1AYoY4rsPwjH4XrTW04DHHQnC" className={styles.resumeBtn}>Resume </a>
+    </div>
+    <img src={getImageUrl("hero/heroImage.png")} alt="My-Image" className={styles.heroImg}/>
+    <div className={styles.topBlur} />
+    <div className={styles.bottomBlur} />
+  </section>
+  );
+}
