61f5882a56ad84d2eb546644c326738565bf5d19
Mohit Kharayat <mohitkharayat0@gmail.com>
2024-07-31 14:08:20 +0530

    contact update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:mohitkharayat0@gmail.com">mohitkharayat0@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
